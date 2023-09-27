6182db682e489ded270d30d941256cbbd0e1ad7a
hyeongeunee <hhg7899@gmail.com>
2023-09-27 18:04:35 +0900

    Front & Back 초기환경 세팅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SideBar from "./SideBar";
+
+function App() {
+    return (
+        <div>
+            <SideBar />
+        </div>
+    );
+}
+
+export default App;
